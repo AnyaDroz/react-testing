3e043a173340a122b4fea3e8b445fe3fd33e40dd
AnyaDroz <anya.drozdova89@gmail.com>
2024-06-23 10:21:22 +0100

    feat: add tests to form

--- src/components/Login.test.js
+++ src/components/Login.test.js
@@ -1,6 +1,15 @@
 import Login from "./Login";
-import {fireEvent, render, screen} from "@testing-library/react";
+import {fireEvent, render, screen, waitFor} from "@testing-library/react";
 
+
+jest.mock("axios", ()=> ({
+    __esModule: true,
+    default:{
+        get: () => ({
+            data:{name: 'John'}
+        })
+    }
+}))
 test("username input should be rendered", ()=> {
     render(<Login/>)
     const userInput = screen.getByPlaceholderText(/username/i);
@@ -61,6 +70,55 @@ test("button should not be disabled when input exists", ()=> {
     fireEvent.change(password, {target:{value:testValue}})
     expect(button).not.toBeDisabled()
 })
+
+test("loading should not be rendered", ()=> {
+    render(<Login/>)
+    const button = screen.getByRole("button")
+    expect(button).not.toHaveTextContent(/please wait/i)
+})
+
+
+test("loading should render when clicked", ()=> {
+    render(<Login/>)
+    const button = screen.getByRole("button")
+    const password = screen.getByPlaceholderText(/password/i)
+    const username = screen.getByPlaceholderText(/username/i)
+    const testValue = "test"
+    fireEvent.change(username, {target:{value:testValue}})
+    fireEvent.change(password, {target:{value:testValue}})
+    fireEvent.click(button)
+    expect(button).toHaveTextContent(/please wait/i)
+})
+
+test("loading should not be rendered after fetching", async ()=> {
+    render(<Login/>)
+    const button = screen.getByRole("button")
+    const password = screen.getByPlaceholderText(/password/i)
+    const username = screen.getByPlaceholderText(/username/i)
+    const testValue = "test"
+    fireEvent.change(username, {target:{value:testValue}})
+    fireEvent.change(password, {target:{value:testValue}})
+    fireEvent.click(button)
+
+    await waitFor(()=>
+    expect(button).not.toHaveTextContent(/please wait/i))
+})
+
+test("user should be rendered after fetching", async ()=> {
+    render(<Login/>)
+    const button = screen.getByRole("button")
+    const password = screen.getByPlaceholderText(/password/i)
+    const username = screen.getByPlaceholderText(/username/i)
+    const testValue = "test"
+    fireEvent.change(username, {target:{value:testValue}})
+    fireEvent.change(password, {target:{value:testValue}})
+    fireEvent.click(button)
+    const userItem = await screen.findByText('John')
+
+    expect(userItem).toBeInTheDocument()
+
+})
+
 
 
 
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,17 +1,43 @@
-import {Fragment, useState} from "react";
+import React, {useState} from "react";
+import axios from "axios";
+
+
+
+type User = {
+    name: string
+}
 
 const Login = () => {
     const [error, setError] = useState(false)
     const [password, setPassword] = useState("")
     const [username, setUsername] = useState("")
+    const [user, setUser] = useState<User>()
+    const [loading, setLoading] = useState(false)
+
+    const handleClick = async (e: React.MouseEvent) => {
+        e.preventDefault();
+        setLoading(true);
+        try {
+            const { data } = await axios.get(
+                "https://jsonplaceholder.typicode.com/users/1"
+            );
+            setUser(data);
+        } catch {
+            setError(true);
+        }
+        setLoading(false)
+    };
+
     return (
         <div className="container">
+            <span>{user ? user.name : ""}</span>
             <form>
                 <input type = "text" placeholder="username" value={username}
                        onChange={(event)=>setUsername(event.currentTarget.value)}/>
                 <input type = "password" placeholder="password" value={password}
                        onChange={(event)=>setPassword(event.currentTarget.value)}/>
-                <button disabled={password ==="" || username ===""}>Login</button>
+                <button disabled={password ==="" || username ===""} onClick={handleClick}> {loading ? "please wait" : "Login"}</button>
+
                 <span data-testid = "error" style={{visibility: error ? "visible" : "hidden"}}>something went wrong</span>
             </form>
         </div>
